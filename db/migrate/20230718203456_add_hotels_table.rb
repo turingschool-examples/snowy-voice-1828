7fe6e7a6efca2be3a9f56279a4c4d9445e0fae16
Juliet Eyraud <juliet@turing.edu>
2023-07-18 15:39:28 -0500

    Setup db with one to many relationship

---
+++ db/migrate/20230718203456_add_hotels_table.rb
@@ -0,0 +1,10 @@
+class AddHotelsTable < ActiveRecord::Migration[7.0]
+  def change
+    create_table :hotels do |t|
+      t.string :name
+      t.string :location
+      
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20230718203652_add_rooms_table.rb
@@ -0,0 +1,11 @@
+class AddRoomsTable < ActiveRecord::Migration[7.0]
+  def change
+    create_table :rooms do |t|
+      t.integer :rate
+      t.string :suite
+      t.references :hotel, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
