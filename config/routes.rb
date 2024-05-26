e4a4bfe95b51aa6d16d5284df4830feb41749700
James McHugh <jdmchugh111@gmail.com>
2024-05-26 19:51:16 -0400

    Wrote and passed tests for User Story 3

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   get "/guests/:id", to: "guests#show"
 
   post "/guests/:id", to: "guest_rooms#create"
+
+  get "/rooms", to: "rooms#index"
 end
--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -6,4 +6,19 @@ RSpec.describe Room, type: :model do
     it { should have_many :guest_rooms }
     it { should have_many(:guests).through(:guest_rooms) }
   end
+
+  describe "instance methods" do
+    describe "#guests_count" do
+      it "can count all guests" do
+        @hotel1 = Hotel.create!(name: 'Echo Mountain Inn', location: 'Echo Mountain')
+        @room1 = Room.create!(rate: 125, suite: "Presidential", hotel_id: @hotel1.id)
+        @guest1 = Guest.create!(name: 'Charlize Theron', nights: 3)
+        @guest2 = Guest.create!(name: 'Tom Hardy', nights: 4)
+        GuestRoom.create(room_id: @room1.id, guest_id: @guest1.id)
+        GuestRoom.create(room_id: @room1.id, guest_id: @guest2.id)
+
+        expect(@room1.guests_count).to eq(2)
+      end
+    end
+  end
 end
