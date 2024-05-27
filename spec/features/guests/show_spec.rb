74c1b02368512cdaf18f6b2bcad8de8b49cba6ef
Austin Carr-Jones <austincarr.jones@gmail.com>
2024-05-27 11:44:03 -0600

    test 1

---
+++ spec/features/guests/show_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+# Story 1
+# Guest Show
+# As a visitor
+# When I visit a guest's show page
+# I see the guest's name
+# And I see a list of all the rooms they've stayed in
+# including the room's suite, nightly rate, and the name of the hotel that it belongs to.
+
+RSpec.describe "the guests show page" do
+  context "as a visitor" do 
+    it "I see the guest's name" do 
+      guest_1 = Guest.create!(name: "Jim James")
+    
+      visit "/guests/#{guest.id}"
+
+      expect(page).to have_content(guest_1.name)
+    end
+  end
+end
