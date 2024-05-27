846e5b4823ef4fcd20c3d40c276b93743e54a428
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-05-26 21:31:43 -0600

    feat: model test for instance methods

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -6,4 +6,20 @@ RSpec.describe Room, type: :model do
     it { should have_many :guest_rooms }
     it { should have_many(:guests).through(:guest_rooms) }
   end
+
+  describe 'instance methods' do
+    it 'can count all guests that have stayed in a room' do
+      @hotel = Hotel.create(name: 'Hilton', location: 'Denver')
+      @room = @hotel.rooms.create(rate: 125, suite: 'presidential', hotel_id: @hotel.id)
+      @room2 = @hotel.rooms.create(rate: 125, suite: 'King', hotel_id: @hotel.id)
+      @guest = Guest.create(name: 'John', nights: 3)
+      @guest2 = Guest.create(name: 'Samantha', nights: 4)
+      @guest3 = Guest.create(name: 'Hank', nights: 2)
+      @guest_room = GuestRoom.create(guest_id: @guest.id, room_id: @room.id)
+      @guest_room2 = GuestRoom.create(guest_id: @guest2.id, room_id: @room.id)
+      @guest_room3 = GuestRoom.create(guest_id: @guest3.id, room_id: @room2.id)
+
+      expect(@room.guest_count).to eq(2)
+    end
+  end
 end
