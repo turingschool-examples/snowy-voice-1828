ba45213dbf6dc337942e480e271a3362e2f7760f
Austin Carr-Jones <austincarr.jones@gmail.com>
2024-05-27 12:32:39 -0600

    test US2

--- spec/features/guests/show_spec.rb
+++ spec/features/guests/show_spec.rb
@@ -30,5 +30,35 @@ RSpec.describe "the guests show page" do
       expect(page).to have_content(room_1.rate)
       expect(page).to have_content(room_1.hotel.name)
     end
+  
+    # Story 2
+    # Add a Guest to a Room
+    # As a visitor
+    # When I visit a guest's show page
+    # Then I see a form to add a room to this guest.
+    # When I fill in a field with the id of an existing room
+    # And I click submit
+    # Then I am redirected back to the guest's show page
+    # And I see the room now listed under this guest's rooms.
+    # (You do not have to test for a sad path, for example if the ID submitted is not an existing room)
+    it "I see a form to add a room to this guest" do
+      guest_1 = Guest.create!(name: "Jim James")
+      hotel_1 = Hotel.create!(name: 'Echo Mountain Inn', location: 'Echo Mountain')
+      room_1 = hotel_1.rooms.create!(rate: 125, suite: "Presidential")
+      hotel_2 = Hotel.create!(name: 'White Lotus', location: 'Maui')
+      room_2 = hotel_1.rooms.create!(rate: 1500, suite: "Pineapple")
+      RoomsGuest.create!(room: room_1, guest: guest_1)
+      
+      visit "/guests/#{guest_1.id}"
+      # save_and_open_page
+      expect(page).to have_content("Add a room")
+			fill_in :room_id, with: "2"
+			click_button("Submit")
+      # save_and_open_page
+      expect(current_path).to eq("/guests/#{guest_1.id}")
+      expect(page).to have_content("White Lotus")
+      expect(page).to have_content("Pineapple")
+    end
+
   end
 end
