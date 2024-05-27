92da7d99e933e3e3137ee8e8128fb5c9520c051c
Jan Lehigh <janlehigh461437@gmail.com>
2024-05-27 13:46:33 -0600

    Add refactored sections of migration.

--- db/migrate/20240527194312_create_guest_rooms.rb
+++ db/migrate/20240527194312_create_guest_rooms.rb
@@ -1,7 +1,8 @@
 class CreateGuestRooms < ActiveRecord::Migration[7.1]
   def change
     create_table :guest_rooms do |t|
-
+      t.integer :guest_id
+      t.integer :room_id
       t.timestamps
     end
   end
