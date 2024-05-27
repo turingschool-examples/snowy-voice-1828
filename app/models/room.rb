e592a535e32c6b487798456ac0cceb9425a09211
Aurora Sanderson <AuroraTuring@gmail.com>
2024-05-27 12:40:48 -0600

    Add relationships

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,4 @@
 class Room < ApplicationRecord
   belongs_to :hotel
+  has_many :guests
 end
---
+++ spec/features/guest/show_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+RSpec.describe "guest show page", type: :feature do
+
+  before(:each) do
+
+  end
+
+end
