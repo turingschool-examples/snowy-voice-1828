106e9b4ad8e391ee56935d1c5be4ead87b619978
Austin Carr-Jones <austincarr.jones@gmail.com>
2024-05-27 12:00:19 -0600

    test2, US1

--- spec/features/guests/show_spec.rb
+++ spec/features/guests/show_spec.rb
@@ -17,5 +17,17 @@ RSpec.describe "the guests show page" do
 
       expect(page).to have_content(guest_1.name)
     end
+
+    it "I see a list of all rooms they've stayed in including suite, rate, and name of hotel it belongs to" do 
+      guest_1 = Guest.create!(name: "Jim James")
+      hotel_1 = Hotel.create!(name: 'Echo Mountain Inn', location: 'Echo Mountain')
+      room_1 = hotel_1.rooms.create!(rate: 125, suite: "Presidential")
+      RoomsGuest.create!(room: room_1, guest: guest_1)
+      visit "/guests/#{guest_1.id}"
+
+      expect(page).to have_content(room_1.suite)
+      expect(page).to have_content(room_1.rate)
+      expect(page).to have_content(room_1.hotel.name)
+    end
   end
 end
