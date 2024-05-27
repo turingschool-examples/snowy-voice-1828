dc0df1eca5eb18d127e07059d1cc393a54328009
Aurora Sanderson <AuroraTuring@gmail.com>
2024-05-27 12:46:04 -0600

    Add guests table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_07_18_203652) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_27_184532) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "guests", force: :cascade do |t|
+    t.string "name"
+    t.integer "nights"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "hotels", force: :cascade do |t|
     t.string "name"
